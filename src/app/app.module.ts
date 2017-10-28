52f2f0a7f0c68cbaacfa67a837c38e2ae280e802
Pakteema <sand_mocha@hotmail.com>
2017-10-28 13:44:26 +0700

    sand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoginService } from './services/login.service' 
+import {AuthguardGuard} from './authguard.guard'
+import { UserManagementService } from './services/user-management.service'
+
+
+
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { FormsModule } from '@angular/forms';
+import { GetphotoService } from './services/getphoto.service'
+import { HttpModule } from '@angular/http'
+import{RouterModule,Routes} from "@angular/router";
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserManagementComponent } from './components/user-management/user-management.component'
+ 
+
+const appRoutes:Routes = [
+  {path: '',component: LoginComponent},
+  {path: 'home',canActivate: [AuthguardGuard],component: HomeComponent},
+  {path: 'user',canActivate: [AuthguardGuard],component: UserComponent},
+  {path: 'aboutus',canActivate: [AuthguardGuard],component: AboutusComponent},
+  {path: 'user-management',canActivate: [AuthguardGuard],component: UserManagementComponent},
+  {path: '**',canActivate: [AuthguardGuard],component: LoginComponent}
+]
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutusComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserManagementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+       HttpModule,
+       RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+   providers: [
+	GetphotoService,
+	LoginService,
+	AuthguardGuard,
+	UserManagementService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/user-management/user-management.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { UserManagementService } from '../../services/user-management.service';
+
+@Component({
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.css']
+})
+export class UserManagementComponent implements OnInit {
+
+private userList:User[];  
+private respone;
+private status:string;
+private isEditable:boolean[]; 
+
+
+  constructor(private userManagementService:UserManagementService) { }
+
+ ngOnInit() {
+
+
+	this.status = "superadmin";
+
+	this.isEditable = [];
+
+	this.getAllUser();
+
+  
+  }
+toggleEdit(i){
+  this.isEditable.forEach((element,index) => {
+    	if(i == index){
+      	this.isEditable[index] = !this.isEditable[index]
+    	}else{
+      	this.isEditable[index] = false;
+    	}
+	});
+	console.log(this.isEditable);
+  }
+
+addUser(username,password,email){
+ this.userManagementService.createNewUser(username,password,email,this.status).subscribe((response) => {
+	this.respone = response;
+	this.getAllUser();
+  });
+}
+
+getAllUser(){
+	this.userManagementService.getAllUser().subscribe((response) => {
+  	this.userList = response;
+  	this.isEditable = [];
+  	for(var i = 0; i < this.userList.length; i++){
+    	this.isEditable.push(false);
+   	}
+   	console.log(this.isEditable);
+	});
+  }
+
+
+deleteUser(user){
+	this.userList.forEach((element,index) => {
+  	if(element == user){
+    	this.userManagementService.deleteUser(user.adm_id).subscribe((response) => {
+      	console.log("delete respone : "+response);
+    	});
+    	this.userList.splice(index,1);
+  	}
+	});
+	return false;
+  }
+
+ editUser(id,username,password,email,status){
+	this.userManagementService.editUser(id,username,password,email,status).subscribe((response) => {
+  	this.getAllUser();
+	});
+  }
+
+}
+
+interface User {
+  adm_id: string;
+  username: string;
+  email: string;
+  password: string;
+  status: string;
+}
